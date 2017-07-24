8aced6faeb1318418bdfc0962bfc3c9a87a4277e
Rika Yoshida <rika.yoshida@nnh.go.jp>
2017-07-24 16:27:10 +0900

    add important to style
    - display:noneが別cssの影響で無視されることがあるのでss保存時にimportantを付与

--- app/js/webview.js
+++ app/js/webview.js
@@ -8,9 +8,15 @@ ipc.on('insert-datetime', (event, arg) => {
   div.style.cssText = 'text-align:right; width:100%; background:white; font-size:15px'
   div.innerText = arg;
   document.getElementById('cover').insertBefore(div, document.getElementById('cover').firstChild);
+
+  // display:noneが別のcssに上書きされて無視されることがあるのでimportantを付与
+  $('*[style*="display: none"]').css({'cssText': 'display: none !important;'});
 });
 
 ipc.on('remove-datetime', (event, arg) => {
   let div = document.getElementById('screenshot-datetime');
   document.getElementById('cover').removeChild(div);
+
+  // 付与したimportantを元に戻す
+  $('*[style*="display: none"]').css('display','none')
 })
