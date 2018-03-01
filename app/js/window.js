de341cd17c26fcf5edfbbe48d664265938dfa1b2
Rika Yoshida <rika.yoshida@nnh.go.jp>
2018-03-01 10:01:46 +0900

    add generate pdf from urls

--- app/js/window.js
+++ app/js/window.js
@@ -18,6 +18,11 @@ let photoButton = null;
 let folderButton = null;
 let folderText = null;
 let saveDirectory = null;
+let prepareButton = null;
+let captureContainer = null;
+let captureText = null;
+let captureButton = null;
+let captureResult = null;
 
 window.addEventListener('load', () => {
   tabGroup = new TabGroup();
@@ -35,6 +40,11 @@ window.addEventListener('load', () => {
   folderButton = document.getElementById('folder-button');
   folderText = document.getElementById('folder-text');
   folderText.innerText = saveDirectory;
+  prepareButton = document.getElementById('prepare-button');
+  captureContainer = document.getElementById('capture-container');
+  captureText = document.getElementById('capture-text');
+  captureButton = document.getElementById('capture-button');
+  captureResult = document.getElementById('capture-result');
 
   addTabbutton.addEventListener('click', () => {
     createTab();
@@ -67,7 +77,19 @@ window.addEventListener('load', () => {
   });
   folderButton.addEventListener('click', () => {
     selectFolder();
-  })
+  });
+  prepareButton.addEventListener('click', () => {
+     if (captureContainer.style['display'] == 'none') {
+       captureContainer.style['display'] = 'block';
+     } else {
+       captureContainer.style['display'] = 'none';
+       captureText.value = '';
+       captureResult.innerHTML = '';
+     }
+  });
+  captureButton.addEventListener('click', () => {
+    if (captureText.value.length > 0) captureFromUrls(captureText.value.split('\n'));
+  });
 });
 
 function createTab(url = 'https://test-ptosh.herokuapp.com') {
@@ -88,9 +110,7 @@ function createTab(url = 'https://test-ptosh.herokuapp.com') {
   });
 }
 
-function savePDF() {
-  let webview = tabGroup.getActiveTab().webview;
-
+function savePDF(webview = tabGroup.getActiveTab().webview, isShowDialog = true, callback = null) {
   let today = new Date();
 
   let offset = today.getTimezoneOffset() / -60;
@@ -100,7 +120,7 @@ function savePDF() {
   webview.send('insert-datetime', isoDatetime);
 
   let date = `${today.getFullYear()}${zeroPadding(today.getMonth()+1)}${zeroPadding(today.getDate())}`;
-  let time = `${zeroPadding(today.getHours())}${zeroPadding(today.getMinutes())}${zeroPadding(today.getSeconds())}`
+  let time = `${zeroPadding(today.getHours())}${zeroPadding(today.getMinutes())}${zeroPadding(today.getSeconds())}`;
   let trialName = webview.src.split('/')[4];
   let sheetName = webview.src.split('/')[8];
   let path = `${saveDirectory}/ptosh_crf_image/${trialName}/${sheetName}/${date}_${time}.pdf`;
@@ -115,16 +135,20 @@ function savePDF() {
     },
     (error, data) => {
       if (error !== null) {
-        showDialog(error.toString, 'error');
+        if (isShowDialog) showDialog(error.toString(), 'error');
+        if (typeof callback === 'function') callback(error.toString());
+        return;
       }
 
       fs.ensureFileSync(path);
       fs.writeFile(path, data, (error) => {
         webview.send('remove-inserted-element');
         if (error === null) {
-          showDialog(`保存しました。\n${path}`, 'info');
+          if (isShowDialog) showDialog(`保存しました。\n${path}`, 'info');
+          if (typeof callback === 'function') callback(path);
         } else {
-          showDialog(error.toString, 'error');
+          if (isShowDialog) showDialog(error.toString(), 'error');
+          if (typeof callback === 'function') callback(error.toString());
         }
       });
     }
@@ -154,5 +178,50 @@ function selectFolder() {
   }, (directories) => {
     saveDirectory = directories[0];
     folderText.innerText = saveDirectory;
+  });
+}
+
+function captureFromUrls(urls) {
+  let result = [];
+  let promices = (urls) => {
+    return Promise.all(urls.map((url) => {
+      return doPromise(url);
+    }));
+  };
+  let doPromise = (url) => {
+    return new Promise((resolve, reject) => {
+      let tab = tabGroup.addTab({
+        title: 'blank',
+        src: url,
+        visible: true
+      });
+      tab.webview.addEventListener('did-stop-loading', () => {
+        savePDF(tab.webview, false, (res) => {
+          result.push({
+            url: url,
+            result: res
+          });
+          tab.close();
+          resolve();
+        });
+      });
+    });
+  }
+  promices(urls).then(() => {
+    let table = document.createElement('table');
+    table.id = 'capture-result-table'
+    let tbody = document.createElement('tbody');
+    for (let i = 0; i < result.length; i++) {
+      let res = result[i];
+      let row = document.createElement('tr');
+      for (let key in res) {
+        let cell = document.createElement('td');
+        cell.innerText = res[key];
+        row.appendChild(cell);
+      }
+      tbody.appendChild(row);
+    }
+    table.appendChild(tbody);
+    captureResult.appendChild(table);
   });
 }
