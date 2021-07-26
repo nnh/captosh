ac6705ce8322c845827c6e8059d099f1222e078f
Katsusuke Shimizu <katsusuke@gmail.com>
2021-07-26 15:14:49 +0900

    bump latest electron

--- src/main.js
+++ src/main.js
@@ -30,7 +30,12 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1024,
     height: 768,
-    webPreferences: { partition: 'persist:ptosh' }
+    webPreferences: {
+      partition: 'persist:ptosh',
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    }
   });
   mainWindow.loadURL(`file://${__dirname}/window.html`);
   // mainWindow.webContents.openDevTools();
