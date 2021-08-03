acb3e0d21b36d09641c397316db84411d9dc564f
Katsusuke Shimizu <katsusuke@gmail.com>
2021-08-03 15:08:19 +0900

    add type

--- src/js/actions/bookmark_actions.ts
+++ src/js/actions/bookmark_actions.ts
@@ -12,19 +12,19 @@ export const getBookmarks = () => {
   return dispatch => getAction(dispatch);
 }
 
-export const selectBookmark = (url) => ({
+export const selectBookmark = (url: string) => ({
   type: BookmarkActionType.select,
   url
 });
 
-export const deleteBookmark = (url) => {
+export const deleteBookmark = (url: string) => {
   return async dispatch => {
     await Bookmark.delete(url);
     getAction(dispatch);
   }
 };
 
-export const addBookmark = (url, title) => {
+export const addBookmark = (url: string, title: string) => {
   return async dispatch => {
     await Bookmark.add(url, title);
     getAction(dispatch);
