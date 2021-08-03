ed4ec61f649e525337838cbeb4e2ce375d35a449
Katsusuke Shimizu <katsusuke@gmail.com>
2021-08-03 15:07:09 +0900

    add type

--- src/js/bookmark.ts
+++ src/js/bookmark.ts
@@ -1,31 +1,38 @@
 import * as util from 'util';
 import * as storage from 'electron-json-storage';
 
+type BookmarkType = {[T: string]: string};
+
+const key = 'bookmark';
+
 export default class Bookmark {
-  static async get() {
+  static async get(): Promise<BookmarkType> {
     const bookmarks = await this.getData();
-    if (Object.keys(bookmarks).length && bookmarks[0] !== null) {
+    if (Object.keys(bookmarks).length && bookmarks['0']) {
       return bookmarks;
     } else {
-      const defaultBookmark = { 'https://builder.ptosh.com': 'builder' };
+      const defaultBookmark: BookmarkType = { 'https://builder.ptosh.com': 'builder' };
       await this.setData(defaultBookmark);
       return defaultBookmark;
     }
   }
 
-  static async add(url, title) {
+  static async add(url: string, title: string) {
     const bookmarks = await this.getData();
     bookmarks[url] = title;
     await this.setData(bookmarks);
   }
 
-  static async delete(url) {
+  static async delete(url: string) {
     const bookmarks = await this.getData();
     delete bookmarks[url];
     await this.setData(bookmarks);
   }
+  static getData() {
+    return util.promisify(storage.get)(key) as Promise<BookmarkType>;
+  }
+  static setData(data: BookmarkType) {
+    return util.promisify(storage.set)(key, data);
+  }
 }
 
-Bookmark.key = 'bookmark';
-Bookmark.getData = () => util.promisify(storage.get)(Bookmark.key);
-Bookmark.setData = (data) => util.promisify(storage.set)(Bookmark.key, data);
