da65c76dcb19e573ef0a7b9d7e2cc0178f401a4b
Katsusuke Shimizu <katsusuke@gmail.com>
2021-08-25 19:06:48 +0900

    remove unicode space

--- src/js/filename.ts
+++ src/js/filename.ts
@@ -1,3 +1,3 @@
 export function sanitizePath(path: string) {
-  return path.replace('\\', '/').replace(/\.\.?\//g, '/').replace(/\\|\:|\*|\?|"|<|>|\||[\f\n\r\t\v​\u00a0\u1680​\u180e\u2000​-\u200a​\u2028\u2029\u202f\u205f​\u3000\ufeff]/g, '_')
+  return path.replace('\\', '/').replace(/\.\.?\//g, '/').replace(/[\\:\*\?"<>\|\f\n\r\t\v\u00a0\u1680\u180e\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]/gu, '_')
 }
