83f1c1374e7db40c01eed5325bc70d40f4cab8c3
Katsusuke Shimizu <katsusuke@gmail.com>
2021-08-25 19:16:44 +0900

    add case

--- __tests__/filename.test.ts
+++ __tests__/filename.test.ts
@@ -1,5 +1,15 @@
 import { sanitizePath } from '../src/js/filename';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sanitizePath('../../bash')).toBe('bash');
+describe('sanitizePath', () => {
+  test('dir safe', () => {
+    expect(sanitizePath('../../bash.exe')).toBe('bash.exe');
+  });
+
+  test('exclude root path', () => {
+    expect(sanitizePath('/bin/bash.exe')).toBe('bin/bash.exe');
+  })
+
+  test('replace invalid caras', () => {
+    expect(sanitizePath('bin/<bash? (1).exe')).toBe('bin/_bash_ (1).exe');
+  })
 });
