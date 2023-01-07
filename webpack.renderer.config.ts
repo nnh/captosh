aa3af68656e0e59a687fea96ff200df91019aa0c
Katsusuke Shimizu <katsusuke@gmail.com>
2023-01-07 15:17:49 +0900

    compile scss and use latest fontawesome and old bootstrap-scss

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -4,8 +4,25 @@ import { rules } from './webpack.rules';
 import { plugins } from './webpack.plugins';
 
 rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  test: /\.(eot|svg|ttf|woff|woff2)$/,
+  use: [{
+    loader: 'file-loader',
+    options: {
+      name: "[path][name].[ext]",
+      publicPath: "../",
+    }
+  }]
+});
+rules.push({
+  test: /\.s[ac]ss(\?v=[^=]+)?$/i,
+  use: [
+    // Creates `style` nodes from JS strings
+    "style-loader",
+    // Translates CSS into CommonJS
+    "css-loader",
+    // Compiles Sass to CSS
+    "sass-loader",
+  ],
 });
 
 export const rendererConfig: Configuration = {
